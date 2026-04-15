765e2a0abf6d2136a43ce6c4505abfe83411d7a4
Virginia <calcetinesazules@hotmail.com>
2026-04-15 16:10:31 +0200

    cerrar scanner

--- src/main/java/com/example/CalculadoraBasica.java
+++ src/main/java/com/example/CalculadoraBasica.java
@@ -75,6 +75,7 @@ public class CalculadoraBasica {
         int suma = calculadora.sumar(num1, num2);
 
         System.out.println("La suma de " + num1 + " y " + num2 + " es: " + suma);
+        sc.close();
     }
 }
 
